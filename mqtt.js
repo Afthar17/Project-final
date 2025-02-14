f49b645665415d391807944be9dde2d59db83545
Afthar17 <aftharafthar5@gmail.com>
2025-02-14 22:10:42 +0530

    mqtt commit

--- mqtt.js
+++ mqtt.js
@@ -1,9 +1,9 @@
 import mqtt from "mqtt";
 
-const mqtt_host = "9bc2c4af80f44ddb8728a6f05ad0a398.s1.eu.hivemq.cloud";
+const mqtt_host = "6d506308d505411e9ef1e1593cd72c1c.s1.eu.hivemq.cloud";
 const mqtt_port = "8883";
-const mqtt_user = "silsil";
-const mqtt_password = "silsil@123";
+const mqtt_user = "afthar";
+const mqtt_password = "Afthar123";
 
 const client = mqtt.connect(`mqtts://${mqtt_host}:${mqtt_port}`, {
   username: mqtt_user,
